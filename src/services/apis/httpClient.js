c26686cc469cc6d0d36e2af818bc3b3d01f8f130
Christophe Rosset <tophe@topheman.com>
2018-04-03 18:55:44 +0200

    feat(httpClient): add retry support

--- src/services/apis/httpClient.js
+++ src/services/apis/httpClient.js
@@ -1,3 +1,50 @@
 import axios from "axios";
+import axiosRetry from "axios-retry";
 
-export const makeClient = axiosConfig => axios.create(axiosConfig);
+const fibonacci = num => {
+  if (num <= 1) return 1;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
+
+/**
+ * If starts === 4, will return:
+ * 1) 500
+ * 2) 800
+ * 3) 1300
+ * ... see fibonnacci sequence
+ *
+ * @param {Number} starts You can adjust this number from where you want to start
+ * @return {Function} To be passed to retryDelay
+ */
+const fibonacciRetryDelay = (starts = 4) => (retryNumber = 0) => {
+  const delay = fibonacci(retryNumber + starts) * 100;
+  const randomSum = delay * 0.2 * Math.random(); // 0-20% of the delay
+  return delay + randomSum;
+};
+
+/**
+ * Returns an axios client
+ *
+ * @param {Object} [options] The config passed to axios.create()
+ * @param @optional {Number} [options.retries] pass 0 if you don't wan't retries
+ * @param @optional {Number} [options.retryDelay]
+ */
+export const makeClient = ({
+  retries = 3,
+  retryDelay = fibonacciRetryDelay(),
+  ...axiosConfig
+}) => {
+  const client = axios.create(axiosConfig);
+  /*
+   * If the network or the api server fails a request, the client will
+   * retry 3 times until the request goes through
+   * Only applied to idempotent requests:
+   * - method GET/HEAD/OPTIONS/PUT/DELETE
+   * - 5xx server error
+   * - network errors
+   */
+  if (retries) {
+    axiosRetry(client, { retries, retryDelay });
+  }
+  return client;
+};
